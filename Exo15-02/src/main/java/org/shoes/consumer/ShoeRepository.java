928d6ccd1aaec814e432608d825a78c7b64bdfa5
Cedric <cedric.campagna@gmail.com>
2022-02-15 21:55:29 +0100

    New class to retrieve shoe from a db Not used yet

---
+++ Exo15-02/src/main/java/org/shoes/consumer/ShoeRepository.java
@@ -0,0 +1,13 @@
+package org.shoes.consumer;
+
+import org.shoes.model.impl.Shoe;
+
+public class ShoeRepository implements ShoeDB {
+
+	@Override
+	public Shoe getId(int nextInt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
